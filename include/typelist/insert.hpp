20aa5c974cace0d8b0b7126cdbcfd1ae2e24a585
Qu, Xing <xqu@telenav.cn>
2019-08-28 15:51:56 +0800

    add typelist insert algorithm and tests.

--- include/typelist/insert.hpp
+++ include/typelist/insert.hpp
@@ -2,8 +2,11 @@
 #pragma once
 
 #include <stdint.h>
-#include "utility.hpp"
-#include "typelist.hpp"
+#include "utility/range.hpp"
+#include "front.hpp"
+#include "pop_front.hpp"
+#include "push_back.hpp"
+#include "push_front.hpp"
 #include "size.hpp"
 
 namespace candy {
@@ -16,12 +19,26 @@ template <
 struct InsertT;
 
 template <typename TL, typename T, uint32_t N>
-struct InsertT<TL, T, N, true>
+using Insert = typename InsertT<TL, T, N>::Type;
+
+template <typename TL, typename T, uint32_t N, bool = (Size<TL>::value == N)>
+struct InsertImplT : PushFrontT<Insert<PopFront<TL>, T, N - 1>, Front<TL>>
 {
-    using Type = TL;
 };
 
 template <typename TL, typename T, uint32_t N>
-using Insert = typename InsertT<TL, T, N>::Type;
+struct InsertImplT<TL, T, N, true> : PushBackT<TL, T>
+{
+};
+
+template <typename TL, typename T>
+struct InsertT<TL, T, 0, true> : PushFrontT<TL, T>
+{
+};
+
+template <typename TL, typename T, uint32_t N>
+struct InsertT<TL, T, N, true> : InsertImplT<TL, T, N>
+{
+};
 
 } // namespace candy
--- test/typelist/insert_test.cpp
+++ test/typelist/insert_test.cpp
@@ -8,13 +8,18 @@ namespace {
 
 struct FirstType;
 struct SecondType;
+struct ThirdType;
 
 using EmptyTL = Typelist<>;
 using FirstTL = Typelist<FirstType>;
 using SecondTL = Typelist<FirstType, SecondType>;
+using ThirdTL = Typelist<SecondType, FirstType>;
+using FourthTL = Typelist<FirstType, ThirdType, SecondType>;
 
-static_assert(std::is_same<Insert<EmptyTL, FirstType, 0>, EmptyTL>::value == true, "");
-static_assert(std::is_same<Insert<FirstTL, SecondType, 1>, FirstTL>::value == true, "");
+static_assert(std::is_same<Insert<EmptyTL, FirstType, 0>, FirstTL>::value == true, "");
+static_assert(std::is_same<Insert<FirstTL, SecondType, 1>, SecondTL>::value == true, "");
+static_assert(std::is_same<Insert<FirstTL, SecondType, 0>, ThirdTL>::value == true, "");
+static_assert(std::is_same<Insert<SecondTL, ThirdType, 1>, FourthTL>::value == true, "");
 
 } // namespace
 } // namespace test
